53c89927221e3ca5ba8a15b6db7ea93a59030c23
Marian Wachala <maniekw12w@gmail.com>
2025-02-04 14:14:47 +0100

    Initial project structure

---
+++ src/main/java/com/marianw12/remitly_internship/RemitlyInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.marianw12.remitly_internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemitlyInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemitlyInternshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marianw12/remitly_internship/RemitlyInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marianw12.remitly_internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitlyInternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
