d24a92a37fe1c7aac72f925025e29a9455a0a52f
Marian Wachala <maniekw12w@gmail.com>
2025-02-04 15:30:45 +0100

    Initial Repository configuration

--- src/main/java/com/marianw12/remitly_internship/repository/SwiftCodeRepository.java
+++ src/main/java/com/marianw12/remitly_internship/repository/SwiftCodeRepository.java
@@ -1,8 +1,9 @@
 package com.marianw12.remitly_internship.repository;
 
+import com.marianw12.remitly_internship.entity.SwiftCodeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SwiftCodeRepository extends JpaRepository {
+public interface SwiftCodeRepository extends JpaRepository<SwiftCodeEntity,String> {
 
 
 }
